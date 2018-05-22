654393a9e7c2e76545dfc77a59d8e7887b6f17bf
syvinec <steven.yvinec@imt-atlantique.net>
2018-05-22 12:15:41 +0200

    button colors and checking if already pressed

--- game_code/board.py
+++ game_code/board.py
@@ -16,28 +16,41 @@ class Table(GridLayout):
 
 	def __init__(self, **kwargs):
 		super(Table, self).__init__(**kwargs)
+		self.turn=0
 		self.cols=3
 		self.size_hint=(0.7,0.7)
 		self.pos_hint={"center_x":0.5,"center_y":0.5}
 		
-		#This returns the row and column of the button presssed
+		#This returns the row and column of the button presssed if not already pressed
 		def callback(button):
 			#print(instance,button)
 			for i in range(9):
-				if button == self.buttons['button%s' %i]:
+				if button == self.buttons['button%s' %i]["obj"]:
 					row=i//self.cols
 					col=i%self.cols
 					#print("row",row,"col",col)
-			print('The button %s (%s,%s) is being pressed' %(button.text,row,col))
-			return row,col
+					#we change colors and return row,col if the button isn't already pressed
+					if not(self.buttons['button%s' %i]["state"]):
+						if not(self.turn%2):
+							button.background_color=(128,0,0,1)
+						else:
+							button.background_color=(0,0,128,1)
+						print('The button %s (%s,%s) is being pressed' %(button.text,row,col))
+						self.turn+=1
+						self.buttons['button%s' %i]["state"]=True
+						return row,col
+					else :
+						print('The button %s (%s,%s) is already pressed' %(button.text,row,col))
 
-		#button dictionnary with name 
+		#button dictionnary with name and state {"button":{"obj":button,"state":boolean}
 		self.buttons={}
 		for i in range(9):   
-			self.add_widget(Button(text=str(i)))
-			self.buttons["button%s" %i]=self.children[0]
-			self.buttons["button%s" %i].bind(on_press=callback)
-			#print("dict",self.buttons)
+			self.add_widget(Button(text=str(i),background_color=(1,1,1,1)))
+			self.buttons["button%s" %i]={}
+			self.buttons["button%s" %i]["obj"]=self.children[0]
+			self.buttons["button%s" %i]["obj"].bind(on_press=callback)
+			self.buttons["button%s" %i]["state"]=False
+			print("dict",self.buttons)
 			#print("children",self.children)
 
 
