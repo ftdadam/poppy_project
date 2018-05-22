fc85dfb35763cd8b661cb018d3ab9e1a8185fafa
syvinec <steven.yvinec@imt-atlantique.net>
2018-05-22 11:28:56 +0200

    board drawn with functionnal buttons

--- game_code/board.py
+++ game_code/board.py
@@ -1,8 +1,8 @@
 import kivy
-kivy.require('1.10.0') # replace with your current kivy version !
+#kivy.require('1.10.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -11,26 +11,34 @@ from kivy.config import Config
 #Configuration de l'input TUIO
 Config.set('input','Reactable','tuio,127.0.0.1:3333')
 
-class Table(FloatLayout):
+class Table(GridLayout):
 
-	mainButton = Button(text="Test1", font_size=14)
-	
-
-	#mainButton.bind(on_press=self.callback)
 
 	def __init__(self, **kwargs):
 		super(Table, self).__init__(**kwargs)
-		self.size_hint = (1,1)    
-		self.add_widget(self.mainButton)
-		self.mainButton.size_hint=(0.05,0.02)
-		self.mainButton.pos_hint={'center_x': 0.5,'center_y': 0.5}
-		self.mainButton.bind(on_press=self.callback)
-        
-        #self.mainLampButton.bind(on_release=self.removeButtons)
+		self.cols=3
+		self.size_hint=(0.7,0.7)
+		self.pos_hint={"center_x":0.5,"center_y":0.5}
+		
+		#This returns the row and column of the button presssed
+		def callback(button):
+			#print(instance,button)
+			for i in range(9):
+				if button == self.buttons['button%s' %i]:
+					row=i//self.cols
+					col=i%self.cols
+					#print("row",row,"col",col)
+			print('The button %s (%s,%s) is being pressed' %(button.text,row,col))
+			return row,col
 
-	def callback(instance,button):
-		#print(instance,button)
-		print('The button <%s> is being pressed' %button.text)
+		#button dictionnary with name 
+		self.buttons={}
+		for i in range(9):   
+			self.add_widget(Button(text=str(i)))
+			self.buttons["button%s" %i]=self.children[0]
+			self.buttons["button%s" %i].bind(on_press=callback)
+			#print("dict",self.buttons)
+			#print("children",self.children)
 
 
 class MyApp(App):
