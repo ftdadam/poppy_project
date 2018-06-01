19bff6ca2e05a96f21aae99e21c1e21af9ea61a0
syvinec <steven.yvinec@imt-atlantique.net>
2018-06-01 11:42:39 +0200

    relaunch

--- game_code/board.py
+++ game_code/board.py
@@ -3,6 +3,7 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -11,15 +12,28 @@ from kivy.config import Config
 #Configuration de l'input TUIO
 Config.set('input','Reactable','tuio,127.0.0.1:3333')
 
+
+
+
 class Table(GridLayout):
 	def __init__(self, **kwargs):
 		super(Table, self).__init__(**kwargs)
+		Window.clearcolor = (1,1, 1, 0)
 		self.turn=0
 		self.cols=3
-		self.size_hint=(0.7,0.7)
-		self.pos_hint={"center_x":0.5,"center_y":0.5}
+		self.size_hint=(0.4,0.80)
+		self.pos_hint={"center_x":0.45,"center_y":0.6}
+		self.spacing=[15,15]
 		
+
 		#This returns the row and column of the button presssed if not already pressed
+		def relaunch(button):
+			self.turn=0
+			for but in self.buttons:
+				self.buttons[but]["state"]=False
+				but.background_color=(1,1,1,1)
+
+
 		def callback(button):
 			#print(instance,button)
 			i = self.buttons[button]["id"]
@@ -41,6 +55,13 @@ class Table(GridLayout):
 				print('The button %s (%s,%s) is already pressed' %(self.buttons[button]["id"],row,col))
 
 		#button dictionnary with name and state {"button":{"id":int,"state":boolean}
+		self.add_widget(Button(background_color=(0,0,0,0)))
+		self.add_widget(Button(text="Relaunch",background_color=(1,1,1,1)))
+		self.children[0].bind(on_press=relaunch)
+		self.add_widget(Button(background_color=(0,0,0,0)))
+		self.add_widget(Button(background_color=(0,0,0,0)))
+		self.add_widget(Button(background_color=(0,0,0,0)))
+		self.add_widget(Button(background_color=(0,0,0,0)))
 		self.buttons={}
 		for i in range(9):   
 			#self.add_widget(Button(text=str(i),background_color=(1,1,1,1)))
@@ -49,7 +70,8 @@ class Table(GridLayout):
 			self.buttons[self.children[0]]={}
 			self.buttons[self.children[0]]["id"]=i
 			self.buttons[self.children[0]]["state"]=False
-			print("dict",self.buttons)
+			print(self.buttons[self.children[0]]["id"],self.children[0].pos)
+			#print("dict",self.buttons)
 			#print("children",self.children)
 
 
